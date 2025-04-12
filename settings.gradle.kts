b8bd8b3ea372253b0e26c9e79f82f901056634b2
Deon_Hargrove <67076151+stunbomb@users.noreply.github.com>
2025-04-12 08:30:28 -0500

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "SmartInvestorsLogic"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+    }
+}
+
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
