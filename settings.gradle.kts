c069fcd60a823d1c14af6d51b4e28de339c23142
Deon_Hargrove <67076151+stunbomb@users.noreply.github.com>
2025-04-13 16:38:42 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
+pluginManagement {
+    repositories {
+        google()                  // 🔹 Needed to resolve Android and Compose plugins
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "SmartInvestorsLogic"
